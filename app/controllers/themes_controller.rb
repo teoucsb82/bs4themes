c0a0698a8a8ae19f6a75c2bbffa9aded01b07b86
Teo Dell'Amico <teo@lenda.com>
2016-02-28 10:57:54 -0800

    Added GA traacking

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,7 +1,7 @@
 class ThemesController < ApplicationController
   def index
     @themes = Theme.all
-    @navbar_classes = 'navbar-light bg-faded'
+    @navbar_classes = 'navbar-dark bg-inverse'
   end
 
   def show
