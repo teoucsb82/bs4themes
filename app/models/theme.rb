9054d8a42a75ae8598174a0e4f3ef4379f524edb
Teo Dell'Amico <teo@lenda.com>
2016-02-28 16:28:17 -0800

    Done with v1

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,6 +1,6 @@
 class Theme
   def self.all
-    all = %w(blackboard default elena lacey leo orion slice).sort
-    live = %w(default slice).sort
+    all = %w(blackboard default leo orion slice v1).sort
+    live = all & %w(default slice v1)
   end
 end
