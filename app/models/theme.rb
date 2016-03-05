fc154e3ddaddc8239a902a32fdb932540b03a1c6
Teo Dell'Amico <teo@lenda.com>
2016-03-04 22:30:40 -0800

    Flatro

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,6 +1,6 @@
 class Theme
   def self.all
-    all = %w(blackboard default leo orion slice lacey glowworm).sort
-    live = all & %w(default slice lacey glowworm)
+    all = %w(blackboard default leo orion slice lacey glowworm flatro).sort
+    live = all & %w(default slice lacey glowworm flatro)
   end
 end
